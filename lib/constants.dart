32f282bf888583956c38606657b883b8365e6e5a
Hashim Niane <68332405+hahimniane@users.noreply.github.com>
2024-08-29 08:37:45 -0400

    fixed some bugs on the entire system that was affecting restaurants from being able to receive orders

---
+++ lib/constants.dart
@@ -0,0 +1,63 @@
+import 'package:foodie_restaurant/model/CurrencyModel.dart';
+
+const FINISHED_ON_BOARDING = 'finishedOnBoarding';
+const COLOR_ACCENT = 0xFF8fd468;
+const COLOR_PRIMARY_DARK = 0xFF2c7305;
+const COLOR_DARK = 0xFF191A1C;
+const DARK_COLOR = 0xff191A1C;
+const DARK_VIEWBG_COLOR = 0xff191A1C;
+const DARK_CARD_BG_COLOR = 0xff242528;
+// ignore: non_constant_identifier_names
+var COLOR_PRIMARY = 0xFFFF683A;
+const FACEBOOK_BUTTON_COLOR = 0xFF415893;
+const COUPON_BG_COLOR = 0xFFFCF8F3;
+const COUPON_DASH_COLOR = 0xFFCACFDA;
+const GREY_TEXT_COLOR = 0xff5E5C5C;
+
+const USERS = 'users';
+const REPORTS = 'reports';
+const FOOD_REVIEW = 'foods_review';
+const VENDORS_CATEGORIES = 'vendor_categories';
+const VENDORS = 'vendors';
+const CREATETABLE = 'tables';
+const Order_Rating = 'foods_review';
+const REVIEW_ATTRIBUTES = "review_attributes";
+
+const VENDOR_ATTRIBUTES = "vendor_attributes";
+const PRODUCTS = 'vendor_products';
+const ORDERS = 'restaurant_orders';
+const COUPONS = "coupons";
+const ORDERS_TABLE = 'booked_table';
+const CONTACT_US = 'ContactUs';
+const OrderTransaction = "order_transactions";
+const SECOND_MILLIS = 1000;
+const MINUTE_MILLIS = 60 * SECOND_MILLIS;
+const HOUR_MILLIS = 60 * MINUTE_MILLIS;
+const SERVER_KEY = 'Replace with your Server key';
+const GOOGLE_API_KEY = 'Replace with your API key';
+
+const ORDER_STATUS_PLACED = 'Order Placed';
+const ORDER_STATUS_ACCEPTED = 'Order Accepted';
+const ORDER_STATUS_REJECTED = 'Order Rejected';
+const ORDER_STATUS_DRIVER_PENDING = 'Driver Pending';
+const ORDER_STATUS_DRIVER_ACCEPTED = 'Driver Accepted';
+const ORDER_STATUS_DRIVER_REJECTED = 'Driver Rejected';
+const ORDER_STATUS_SHIPPED = 'Order Shipped';
+const ORDER_STATUS_IN_TRANSIT = 'In Transit';
+const ORDER_STATUS_COMPLETED = 'Order Completed';
+
+const USER_ROLE_VENDOR = 'vendor';
+const STORY = 'story';
+
+const Currency = 'currencies';
+String symbol = '';
+bool isRight = false;
+String currName = "";
+int decimal = 2;
+bool isDineInEnable = false;
+CurrencyModel? currencyData;
+
+const Setting = 'settings';
+String placeholderImage = '';
+const Wallet = "wallet";
+const Payouts = "payouts";
